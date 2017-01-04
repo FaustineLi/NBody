f5201ce0b526b796f40af0ab0daa119f1f5d69cb
Faustine Li <faustineli12@gmail.com>
2017-01-04 00:01:47 -0500

    added force calc methods

--- Planet.java
+++ Planet.java
@@ -29,7 +29,52 @@ public class Planet {
 	}
 
 	public double calcDistance(Planet p) {
-		double dist = Math.sqrt(p.myXPos * p.myXPos + p.myYPos * p.myYPos);
+		double dx = myXPos - p.myXPos; 
+		double dy = myYPos - p.myYPos;
+		double dist = Math.sqrt(dx * dx + dy * dy);
 		return dist;
+	}
+
+	public double calcForceExertedBy(Planet p) {
+		double g = 6.67e-11;
+		double r = calcDistance(p);
+		double force = g * myMass * p.myMass / (r * r);
+		return force;
+	}
+
+	public double calcForceExertedByX(Planet p) {
+		double force = calcForceExertedBy(p);
+		double dx = p.myXPos - myXPos;
+		double r = calcDistance(p);
+		double fx = dx * force / r;
+		return fx;
+	}
+
+	public double calcForceExertedByY(Planet p) {
+		double force = calcForceExertedBy(p);
+		double dy = p.myYPos - myYPos;
+		double r = calcDistance(p);
+		double fx = dy * force / r;
+		return fx;
+	}
+
+	public double calcNetForceExertedByX(Planet[] planets) {
+		double net = 0;
+		for (Planet p : planets) {
+			if (! p.equals(this)) {
+				net += calcForceExertedByX(p);
+			}
+		}
+		return net;
+	}
+
+	public double calcNetForceExertedByY(Planet[] planets) {
+		double net = 0;
+		for (Planet p : planets) {
+			if (! p.equals(this)) {
+				net += calcForceExertedByY(p);
+			}
+		}
+		return net;
 	}
 }
